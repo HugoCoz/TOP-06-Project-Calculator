dc7fce080b0942db48911fd941f673269248cba1
Hugo <cozichugo95@gmail.com>
2022-06-28 23:58:46 +0200

    need to fix the function result()

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 let liveNum = "";
+let historyNum = "";
+let operand = "";
 
-const live = document.querySelector('.live');
+const liveInput = document.querySelector('.live');
+const historyInput = document.querySelector('.history');
 const numberBtn = document.querySelectorAll('div.inputs > div.number');
 const deleteBtn = document.querySelector('div.delete');
 const clearBtn = document.querySelector('div.clear');
+const operandBtn = document.querySelectorAll('div.inputs > div.operand');
+const enterBtn = document.querySelector('.enter');
 
 numberBtn.forEach((e) => {
     e.addEventListener('click', () => {
@@ -12,23 +17,66 @@ numberBtn.forEach((e) => {
 });
 
 deleteBtn.addEventListener('click', () => deleteNumber());
+
 clearBtn.addEventListener('click', () => clearNumber());
+
+operandBtn.forEach((e) => {
+    e.addEventListener('click', () => {
+        moveHistory(liveNum);
+        clearNumber();
+        operand = e.innerHTML;
+    });
+});
+
+enterBtn.addEventListener('click', () => result(liveNum, historyNum, operand));
 
 function addNumber(input) {
     liveNum = liveNum + input;
-    live.innerHTML = liveNum;
+    liveInput.innerHTML = liveNum;
     return liveNum;
 };
 
 function deleteNumber() {
     const del = liveNum.slice(0, -1);
-    live.innerHTML = del;
+    liveInput.innerHTML = del;
     liveNum = del
     return liveNum;
 }
 
 function clearNumber() {
     liveNum = "";
-    live.innerHTML = liveNum;
+    historyNum = "";
+    liveInput.innerHTML = liveNum;
+    historyInput.innerHTML = historyNum
     return liveNum;
+}
+
+function moveHistory(liveNum) {
+    historyNum = liveNum;
+    historyInput.innerHTML = historyNum;
+    return historyNum;
+}
+
+function result(liveNum, historyNum, operand) {
+    let res = 0;
+    switch(operand) {
+        case "+":
+            res = parseFloat(liveNum) + parseFloat(historyNum);
+            break;
+        case "-":
+            res = parseFloat(liveNum) - parseFloat(historyNum);
+            break;
+        case "*":
+            res = parseFloat(liveNum) * parseFloat(historyNum);
+            break;
+        case "/":
+            res = parseFloat(liveNum) / parseFloat(historyNum);
+            break;
+        default:
+            res = "Incorrect value";
+            break;
+    }
+    clearNumber();
+    liveInput.innerHTML = res;
+    return res;
 }
